2db4cfad151de1d700627e1efce2a0268a4d4210
essahatim <hatimessabagh@gmail.com>
2024-01-22 06:06:15 -0800

    fixed error

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,15 +7,16 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i = 0, j = 0;
+	size_t i, j;
 	int temp;
 
 	if (!array || !size)
 		return;
 
+	i = 0;
 	while (i < size)
 	{
-		for (; j < size - 1; j++)
+		for (j = 0; j < size - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
