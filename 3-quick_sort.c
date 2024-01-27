0a59df1340c87f61d11b85b25fb79336334f3d14
essahatim <hatimessabagh@gmail.com>
2024-01-27 13:53:46 -0800

    fixed error

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -9,9 +9,12 @@ void swap(int *a, int *b)
 {
 	int temp;
 
-	temp = *a;
-	*a = *b;
-	*b = temp;
+	if (*a != *b)
+	{
+		temp = *a;
+		*a = *b;
+		*b = temp;
+	}
 }
 
 /**
@@ -25,20 +28,20 @@ void swap(int *a, int *b)
  */
 int lomuto_partition(int *array, size_t size, int low, int high)
 {
-	int i = low - 1, j, pivot = array[high];
+	int i = low, j, pivot = array[high];
 
-	for (j = low; j <= high - 1; j++)
+	for (i = j = low; j <= high; j++)
 	{
 		if (array[j] < pivot)
 		{
-			i++;
 			swap(&array[i], &array[j]);
-			print_array(array, size);
+			print_array((const int *)array, size);
+			i++;
 		}
 	}
-	swap(&array[i + 1], &array[high]);
-	print_array(array, size);
-	return (i + 1);
+	swap(&array[i], &array[high]);
+	print_array((const int *)array, size);
+	return (i);
 }
 
 /**
@@ -55,8 +58,8 @@ void quickSort(int *array, size_t size, int low, int high)
 	if (low < high)
 	{
 		partition_i = lomuto_partition(array, size, low, high);
-		quickSort(array, low, partition_i - 1, size);
-		quickSort(array, partition_i + 1, high, size);
+		quickSort(array, size, low, partition_i - 1);
+		quickSort(array, size, partition_i + 1, high);
 	}
 }
 
@@ -69,5 +72,5 @@ void quick_sort(int *array, size_t size)
 {
 	if (array == NULL || !size)
 		return;
-	quickSort(array, 0, size - 1, size);
+	quickSort(array, size, 0, size - 1);
 }
